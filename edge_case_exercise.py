28807589bd8e7c7e095a31dadaddc2977307788f
choshen0502504479-hue <choshen0502504479@gmail.com>
2025-11-12 19:32:41 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -3,13 +3,14 @@ def move(my_list, direction):
     # Finds the index of the one in the list
     index_of_one = my_list.index(1)
 
-    # Move the one to the left or to the right
-    if direction == 'right':
-        my_list[index_of_one] = 0
-        my_list[index_of_one + 1] = 1
-
-    elif direction == 'left':
-        my_list[index_of_one] = 0
-        my_list[index_of_one - 1] = 1
+    if direction == 'right' :
+        if idx < len(my_list) -1 :
+            my_list[idx] = 0
+            my_list[idx + 1] = 1
+    elif direction == 'left' : 
+        if idx > 0 :
+            my_list[idx] = 0
+            my_list[idx -1 ] = 1
+ 
 
     return my_list
